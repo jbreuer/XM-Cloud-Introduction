94cc76306e900310376c00668e7ef72cdb315640
Jeroen Breuer <jbreuer@gmail.com>
2024-05-01 13:10:36 +0200

    Added Mvp.Project.MvpSite.Layout.

---
+++ src/Project/MvpSite/layout/Controllers/DefaultController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mvp.Project.MvpSite.Middleware;
+using Mvp.Project.MvpSite.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Okta.AspNetCore;
+using Sitecore.AspNet.RenderingEngine;
+using Sitecore.LayoutService.Client;
+using Sitecore.LayoutService.Client.Exceptions;
+using Sitecore.LayoutService.Client.Newtonsoft;
+using Sitecore.LayoutService.Client.Newtonsoft.Converters;
+using Sitecore.LayoutService.Client.Newtonsoft.Model;
+using Sitecore.LayoutService.Client.Request;
+using Sitecore.LayoutService.Client.Response.Model;
+using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Mvp.Project.MvpSite.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ISitecoreLayoutClient _layoutClient;
+        private readonly ISitecoreLayoutSerializer _serializer;
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ISitecoreLayoutClient layoutClient, ISitecoreLayoutSerializer serializer, ILogger<DefaultController> logger)
+        {
+            _layoutClient = layoutClient;
+            _serializer = serializer;
+            _logger = logger;
+        }
+        
+        // Inject Sitecore rendering middleware for this controller action
+        // (enables model binding to Sitecore objects such as Route,
+        // and causes requests to the Sitecore Layout Service for controller actions)
+        [UseMvpSiteRendering]
+        public async Task<IActionResult> Index(LayoutViewModel model)
+        {
+            IActionResult result = null;
+            ISitecoreRenderingContext request = HttpContext.GetSitecoreRenderingContext();
+            
+            // request.Response.Content.Sitecore.Route.Placeholders
+
+            // var req = request.Response.Content.Sitecore.Route.Placeholders.TryGetValue("main", out var main);
+            //
+            // Placeholder mainPlaceholder = main as Placeholder;
+            // foreach (Component component in mainPlaceholder)
+            // {
+            //     var fields = component.Fields;
+            //
+            //     if (fields.TryGetValue("HeroImage", out var fieldReader))
+            //     {
+            //         var heroImage = fieldReader.Read<ImageField>(); 
+            //     }
+            //     
+            //     if (fields.TryGetValue("HeroTitle", out var fieldReaderHeroTitle))
+            //     {
+            //         var heroTitle = fieldReaderHeroTitle.Read<TextField>();
+            //     }
+            // }
+            //
+            var sitecoreLayoutRequest = new SitecoreLayoutRequest
+            {
+                { "sc_site", "mvp-site" },
+                { "sc_apikey", "{E2F3D43E-B1FD-495E-B4B1-84579892422A}" },
+                { "item", "/About" },
+                { "sc_lang", "en" }
+            };
+            var test = await _layoutClient.Request(sitecoreLayoutRequest);
+            //
+            // test.Content.Sitecore.Route.Placeholders.TryGetValue("main", out var mainAbout);
+            //
+            // Placeholder mainPlaceholderAbout = mainAbout as Placeholder;
+            // if (mainPlaceholderAbout != null) 
+            // {
+            //     foreach (Component component in mainPlaceholderAbout)
+            //     {
+            //         var fields = component.Fields;
+            //         
+            //         // Ensure the component has a HeroSubtitle field
+            //         if (component.Fields.ContainsKey("HeroSubtitle"))
+            //         {   
+            //             if (component.Fields.TryGetValue("HeroSubtitle", out var fieldReaderHeroSubtitle))
+            //             {
+            //                 var heroSubtitle = fieldReaderHeroSubtitle.Read<TextField>();
+            //                 if (heroSubtitle != null)
+            //                 {
+            //                     // Create a new JToken with the updated subtitle
+            //                     JToken subtitleToken = JToken.FromObject(new { value = heroSubtitle.Value + " updated text" });
+            //
+            //                     // Use the existing serializer or create a new one if necessary
+            //                     JsonSerializer serializer = new JsonSerializer();
+            //
+            //                     // Create a new NewtonsoftFieldReader with the new JToken
+            //                     NewtonsoftFieldReader newFieldReader = new NewtonsoftFieldReader(serializer, subtitleToken);
+            //
+            //                     // Update the component's Fields dictionary
+            //                     component.Fields["HeroSubtitle"] = newFieldReader;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("mainAbout is not a Placeholder or is null");
+            // }
+            //
+            //
+            // // Console.WriteLine(json);
+            //
+            // var jsonSettings = new JsonSerializerSettings {
+            //     Formatting = Formatting.Indented,
+            //     DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            //     DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            //     ContractResolver = CustomDataContractResolver.Instance,
+            //     Converters = new List<JsonConverter> {
+            //         new NewtonsoftFieldReaderJsonConverter()
+            //     }
+            // };
+            //
+            // var json = JsonConvert.SerializeObject(test.Content, jsonSettings);
+            // return Content(json, "application/json");
+
+            // var test = Sitecore.LayoutService.Client.DefaultLayoutClient
+            
+            if (request.Response?.HasErrors ?? false)
+            {
+                foreach (SitecoreLayoutServiceClientException error in request.Response.Errors)
+                {
+                    switch (error)
+                    {
+                        default:
+                            _logger.LogError(error, error.Message);
+                            throw error;
+                    }
+                }
+            }
+            else if (!(HttpContext.User.Identity?.IsAuthenticated ?? false) && IsSecurePage(request) && !(request.Response?.Content?.Sitecore?.Context?.IsEditing ?? false))
+            {
+                AuthenticationProperties properties = new()
+                {
+                    RedirectUri = HttpContext.Request.GetEncodedUrl()
+                };
+
+                result = Challenge(properties, OktaDefaults.MvcAuthenticationScheme);
+            }
+            else
+            {
+                result = View(model);
+            }
+
+            return result;
+        }
+
+        private static bool IsSecurePage(ISitecoreRenderingContext request)
+        {
+            bool result = false;
+            if (request.Response?.Content?.Sitecore?.Route?.Fields.TryGetValue("RequiresAuthentication", out IFieldReader requiresAuthFieldReader) ?? false)
+            {
+                result = requiresAuthFieldReader.Read<CheckboxField>().Value;
+            }
+            
+            return result;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new LayoutViewModel
+            {
+                MenuTitle = new TextField("Error")
+            });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Healthz()
+        {
+            // TODO: Do we want to add logic here to confirm connectivity with SC etc?
+            return Ok("Healthy");
+        }
+    }
+}
---
+++ src/Project/MvpSite/layout/wwwroot/js/mvp-site.js
@@ -0,0 +1,99 @@
+﻿function timelineEventsHeightRecalculation($timeline, reset) {
+    $timeline.each(function (index, item) {
+        const $item = $(item);
+        let $prev;
+            
+        $item.find(".t-event").each(function (i, e) {
+            let $e = $(e);
+            if (reset) {
+                $e.css("height", "");
+            }
+            else {                
+                if (i % 3 === 0 && $e.next().length > 0) {
+                    $e.height(50);
+                }
+                else if (i % 3 === 1) {
+                    let prevCard = $prev.children(".card").height();
+                    $e.height(prevCard);
+                }
+
+                $prev = $e;
+            }            
+        });            
+    });
+}
+
+$(document).ready(function () {
+    const $window = $(window);
+
+
+    $("#search").keyup(function (event) {
+        if (event.which === 13) {
+            event.preventDefault();
+            $("#gosearch").click();
+        }
+    });
+
+
+    const $addMoreReviewers = $("#addMoreReviewers");
+    if ($addMoreReviewers.length > 0) {
+        const formGroupHtml = $("#addReviewerForm div.form-group")[0].outerHTML;
+        $addMoreReviewers.click(function () {
+            $addMoreReviewers.before(formGroupHtml);
+        });
+    }
+
+
+    const $countdowns = $(".countdown");
+    if ($countdowns.length > 0) {
+        $countdowns.each(function (index, item) {
+            const $item = $(item);
+            const countDownDate = new Date($item.data("date"));
+            const $days = $item.children(".countdown-days");
+            const $hours = $item.children(".countdown-hours");
+            const $minutes = $item.children(".countdown-minutes");
+            const $seconds = $item.children(".countdown-seconds");
+
+            const x = setInterval(function () {
+                let now = new Date().getTime();
+                let nowUTC = new Date(now + new Date().getTimezoneOffset() * 60000);
+                let distance = countDownDate - nowUTC;
+                let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                $days.text(days);
+                $hours.text(hours);
+                $minutes.text(minutes);
+                $seconds.text(seconds);
+
+                if (distance <= 0) {
+                    clearInterval(x);
+                    $item.html("<span class='badge badge-danger'>Expired!</span>");
+                }
+            }, 1000);
+        });
+    }
+
+
+    const $directory = $(".mvp-fp-directory");
+    if ($directory.length > 0) {
+        const $form = $directory.find("form");
+        $form.on("change", "input:checkbox", () => { $form.submit(); });
+    }
+
+
+    const $timeline = $(".timeline");
+    if ($timeline.length > 0) {
+        setTimeout(timelineEventsHeightRecalculation($timeline, $window.width() < 576), 1000);
+        setTimeout(timelineEventsHeightRecalculation($timeline, $window.width() < 576), 2000);
+        setTimeout(timelineEventsHeightRecalculation($timeline, $window.width() < 576), 3000);
+        setTimeout(timelineEventsHeightRecalculation($timeline, $window.width() < 576), 5000);
+        setTimeout(timelineEventsHeightRecalculation($timeline, $window.width() < 576), 10000);
+        
+        $window.on("resize", function () {
+            timelineEventsHeightRecalculation($timeline, $window.width() < 576);
+        });
+    }
+});
