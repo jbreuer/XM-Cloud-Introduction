a086ee5498d26f0644a5c443014bdc56d2ad79e6
Jeroen Breuer <jbreuer@gmail.com>
2024-05-01 17:15:23 +0200

    Experiment to test layoutServiceFactory.

--- src/Project/Sugcon/SugconAnzSxa/src/components/Sugcon/Agenda.tsx
+++ src/Project/Sugcon/SugconAnzSxa/src/components/Sugcon/Agenda.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   withDatasourceCheck,
   GetStaticComponentProps,
@@ -9,6 +8,8 @@ import {
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { fetchSessionizeData } from 'lib/sessionize/fetch-sessonize-data';
 import { ComponentData } from 'lib/sessionize/sessionizeData';
+import { layoutServiceFactory } from 'lib/layout-service-factory';
+import React, { useEffect, useState } from 'react';
 
 interface Fields {
   SessionizeURL: Field<string>;
@@ -31,14 +32,35 @@ export const getStaticProps: GetStaticComponentProps = async (rendering: AgendaR
 
 export const Default = (props: ComponentData): JSX.Element => {
   const externalData = useComponentProps<string>(props.rendering.uid);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const layoutService = layoutServiceFactory.create("sugconanz");
+      try {
+        const data = await layoutService.fetchLayoutData("/");
+        console.log('data', data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);  // Empty dependency array means this effect will only run once after the initial render
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="container component">
-      <h1 className="p-3">
-        <Text field={props?.fields?.Title} />
-      </h1>
-      <div dangerouslySetInnerHTML={{ __html: externalData as string }} />
-    </div>
+      <div className="container component">
+        <h1 className="p-3">
+          <Text field={props?.fields?.Title} />
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: externalData }} />
+      </div>
   );
 };
 
