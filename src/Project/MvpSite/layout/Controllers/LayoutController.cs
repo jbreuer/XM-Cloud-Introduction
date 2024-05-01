aca171c61ec01c091bb9f3a51b51fcbaf127bd8d
Jeroen Breuer <jbreuer@gmail.com>
2024-05-01 13:29:10 +0200

    LayoutController now works as the LayoutService.

--- src/Project/MvpSite/layout/Controllers/LayoutController.cs
+++ src/Project/MvpSite/layout/Controllers/LayoutController.cs
@@ -1,11 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sitecore.LayoutService.Client;
+using Sitecore.LayoutService.Client.Newtonsoft;
+using Sitecore.LayoutService.Client.Request;
 
 namespace Mvp.Project.MvpSite.Controllers;
 
 public class LayoutController : Controller
 {
-    public IActionResult Index()
+    private readonly ISitecoreLayoutClient _layoutClient;
+
+    public LayoutController(ISitecoreLayoutClient layoutClient)
     {
-        return Content("testing");
+        _layoutClient = layoutClient;
+    }
+    
+    public async Task<IActionResult> Index(string item, string sc_apikey, string sc_site, string sc_lang)
+    {
+        var sitecoreLayoutRequest = new SitecoreLayoutRequest
+        {
+            { "sc_site", sc_site },
+            { "sc_apikey", sc_apikey },
+            { "item", item },
+            { "sc_lang", sc_lang }
+        };
+        var test = await _layoutClient.Request(sitecoreLayoutRequest);
+        
+        var jsonSettings = new JsonSerializerSettings {
+            Formatting = Formatting.Indented,
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            ContractResolver = CustomDataContractResolver.Instance,
+            Converters = new List<JsonConverter> {
+                new NewtonsoftFieldReaderJsonConverter()
+            }
+        };
+        
+        var json = JsonConvert.SerializeObject(test.Content, jsonSettings);
+        return Content(json, "application/json");
     }
 }
