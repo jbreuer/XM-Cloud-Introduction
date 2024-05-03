0cb11da4a7243ab0752bb48b1ee58537318aaed9
Jeroen Breuer <jbreuer@gmail.com>
2024-05-03 11:43:51 +0200

    Experiment with IGraphQLClient.

--- src/Project/MvpSite/layout/Controllers/GraphController.cs
+++ src/Project/MvpSite/layout/Controllers/GraphController.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using GraphQL;
+using GraphQL.Client.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Sitecore.LayoutService.Client;
+using Sitecore.LayoutService.Client.Exceptions;
 using Sitecore.LayoutService.Client.Newtonsoft;
 using Sitecore.LayoutService.Client.Newtonsoft.Model;
 using Sitecore.LayoutService.Client.Request;
+using Sitecore.LayoutService.Client.RequestHandlers.GraphQL;
+using Sitecore.LayoutService.Client.Response;
 using Sitecore.LayoutService.Client.Response.Model;
 using Sitecore.LayoutService.Client.Response.Model.Fields;
 
@@ -16,25 +22,30 @@ namespace Mvp.Project.MvpSite.Controllers;
 
 public class GraphController : Controller
 {
-    private readonly ISitecoreLayoutClient _layoutClient;
+    private readonly IGraphQLClient _client;
+    private readonly ISitecoreLayoutSerializer _serializer;
 
-    public GraphController(ISitecoreLayoutClient layoutClient)
+    public GraphController(IGraphQLClient client, ISitecoreLayoutSerializer serializer)
     {
-        _layoutClient = layoutClient;
+        this._client = Assert.ArgumentNotNull<IGraphQLClient>(client, nameof (client));
+        this._serializer = Assert.ArgumentNotNull<ISitecoreLayoutSerializer>(serializer, nameof (serializer));
     }
     
     public async Task<IActionResult> Index([FromBody] GraphQLRequest request)
     {
-        var sitecoreLayoutRequest = new SitecoreLayoutRequest
+        GraphQLResponse<LayoutQueryResponse> graphQlResponse = await this._client.SendQueryAsync<LayoutQueryResponse>(request, new CancellationToken()).ConfigureAwait(false);
+        string str = graphQlResponse?.Data?.Layout?.Item?.Rendered.ToString();
+        var content = this._serializer.Deserialize(str);
+
+        var sitecoreLayoutRequest = new SitecoreLayoutRequest();
+        List<SitecoreLayoutServiceClientException> errors = new List<SitecoreLayoutServiceClientException>();
+        SitecoreLayoutResponse sitecoreLayoutResponse = new SitecoreLayoutResponse(sitecoreLayoutRequest, errors)
         {
-            { "sc_site", "mvp-site" },
-            { "sc_apikey", "{E2F3D43E-B1FD-495E-B4B1-84579892422A}" },
-            { "item", "/About" },
-            { "sc_lang", "en" }
+            Content = content,
+            Metadata = new Dictionary<string, string>().ToLookup<KeyValuePair<string, string>, string, string>((Func<KeyValuePair<string, string>, string>) (k => k.Key), (Func<KeyValuePair<string, string>, string>) (v => v.Value))
         };
-        var test = await _layoutClient.Request(sitecoreLayoutRequest);
         
-        test.Content.Sitecore.Route.Placeholders.TryGetValue("main", out var mainAbout);
+        sitecoreLayoutResponse.Content.Sitecore.Route.Placeholders.TryGetValue("main", out var mainAbout);
         
         Placeholder mainPlaceholderAbout = mainAbout as Placeholder;
         if (mainPlaceholderAbout != null) 
@@ -78,7 +89,7 @@ public class GraphController : Controller
             }
         };
         
-        var json = JsonConvert.SerializeObject(test.Content, jsonSettings);
+        var json = JsonConvert.SerializeObject(sitecoreLayoutResponse.Content, jsonSettings);
         return Content(json, "application/json");
     }
     
