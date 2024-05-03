f593567b29c6b852352d20f195af96a7514f9b8d
Jeroen Breuer <jbreuer@gmail.com>
2024-05-03 15:55:34 +0200

    Working GraphQL example with content changes!

--- src/Project/MvpSite/layout/Controllers/GraphController.cs
+++ src/Project/MvpSite/layout/Controllers/GraphController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
@@ -7,13 +8,25 @@ using GraphQL.Client.Abstractions.Websocket;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.SystemTextJson;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Sitecore.LayoutService.Client;
+using Sitecore.LayoutService.Client.Newtonsoft;
+using Sitecore.LayoutService.Client.Newtonsoft.Model;
 using Sitecore.LayoutService.Client.RequestHandlers.GraphQL;
+using Sitecore.LayoutService.Client.Response.Model;
+using Sitecore.LayoutService.Client.Response.Model.Fields;
 
 namespace Mvp.Project.MvpSite.Controllers;
 
 public class GraphController : Controller
-{   
+{
+    private readonly ISitecoreLayoutSerializer _serializer;
+
+    public GraphController(ISitecoreLayoutSerializer serializer)
+    {
+        this._serializer = Assert.ArgumentNotNull<ISitecoreLayoutSerializer>(serializer, nameof (serializer));
+    }
+    
     public async Task<IActionResult> Index([FromBody] GraphQLRequest request)
     {
         GraphQLHttpClient client = new GraphQLHttpClient("https://xmcloudcm.localhost/sitecore/api/graph/edge", (IGraphQLWebsocketJsonSerializer) new SystemTextJsonSerializer());
@@ -26,6 +39,65 @@ public class GraphController : Controller
         };
         
         GraphQLResponse<LayoutQueryResponse> graphQlResponse = await client.SendQueryAsync<LayoutQueryResponse>(graphqlRequest, new CancellationToken()).ConfigureAwait(false);
+        string str = graphQlResponse?.Data?.Layout?.Item?.Rendered.ToString();
+        var content = this._serializer.Deserialize(str);
+        
+        content.Sitecore.Route.Placeholders.TryGetValue("main", out var mainAbout);
+        
+        Placeholder mainPlaceholderAbout = mainAbout as Placeholder;
+        if (mainPlaceholderAbout != null) 
+        {
+            foreach (Component component in mainPlaceholderAbout)
+            {
+                var fields = component.Fields;
+                
+                // Ensure the component has a HeroSubtitle field
+                if (component.Fields.ContainsKey("HeroSubtitle"))
+                {   
+                    if (component.Fields.TryGetValue("HeroSubtitle", out var fieldReaderHeroSubtitle))
+                    {
+                        var heroSubtitle = fieldReaderHeroSubtitle.Read<TextField>();
+                        if (heroSubtitle != null)
+                        {
+                            // Create a new JToken with the updated subtitle
+                            JToken subtitleToken = JToken.FromObject(new { value = heroSubtitle.Value + " updated text" });
+        
+                            // Use the existing serializer or create a new one if necessary
+                            Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+        
+                            // Create a new NewtonsoftFieldReader with the new JToken
+                            NewtonsoftFieldReader newFieldReader = new NewtonsoftFieldReader(serializer, subtitleToken);
+        
+                            // Update the component's Fields dictionary
+                            component.Fields["HeroSubtitle"] = newFieldReader;
+                        }
+                    }
+                }
+            }
+        }
+        
+        var jsonSettingsNewton = new Newtonsoft.Json.JsonSerializerSettings {
+            Formatting = Newtonsoft.Json.Formatting.Indented,
+            DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat,
+            DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc,
+            ContractResolver = CustomDataContractResolver.Instance,
+            Converters = new List<Newtonsoft.Json.JsonConverter> {
+                new NewtonsoftFieldReaderJsonConverter()
+            }
+        };
+        
+        string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(content, jsonSettingsNewton);;
+
+        // Parse JSON string to JsonDocument
+        using JsonDocument doc = JsonDocument.Parse(jsonString);
+
+        // Extract JsonElement
+        JsonElement jsonElement = doc.RootElement.Clone();
+
+        if (graphQlResponse?.Data?.Layout?.Item != null)
+        {
+            graphQlResponse.Data.Layout.Item.Rendered = jsonElement;
+        }
         
         var jsonSettings = new JsonSerializerOptions
         {
@@ -34,7 +106,7 @@ public class GraphController : Controller
             Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
-
+        
         var json = JsonSerializer.Serialize(graphQlResponse, jsonSettings);
 
         return Content(json, "application/json");
