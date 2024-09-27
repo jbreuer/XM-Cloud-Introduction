204e67cad635a0cfccbb6dc9684e55f28f13f514
Jeroen Breuer <jbreuer@gmail.com>
2024-09-27 17:18:16 +0200

    Revert "Experiment to use SSR."
    This reverts commit 3603a49779132653d6092f8c061d52930e074289.

--- src/Project/Sugcon2024/Sugcon/src/components/Events/Agenda.tsx
+++ src/Project/Sugcon2024/Sugcon/src/components/Events/Agenda.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import InnerHTML from 'dangerously-set-html-content';
-import {
-  Field,
-  GetServerSideComponentProps, GetStaticComponentProps,
-  useComponentProps,
-  withDatasourceCheck
-} from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import useSWR from 'swr';
 import { ComponentProps } from 'lib/component-props';
 
@@ -18,26 +13,20 @@ type AgendaProps = ComponentProps & {
 };
 
 const AgendaDefaultComponent = (props: AgendaProps): JSX.Element => (
-    <div className={`component promo ${props.params.styles}`}>
-      <div className="component-content">
-        <span className="is-empty-hint">Agenda</span>
-      </div>
+  <div className={`component promo ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Agenda</span>
     </div>
+  </div>
 );
 
 const AgendaComponent = (props: AgendaProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
-  console.log('props.rendering.uid', props.rendering.uid);
-  
-  const componentProps = useComponentProps(props.rendering.uid) || {};
-
-  console.dir(componentProps, { depth: null });
-
   const fetcher = (url: string) => fetch(url).then((res) => res.text());
   const { data, error } = useSWR(
-      props.fields.SessionizeUrl.value ? props.fields.SessionizeUrl.value : null,
-      fetcher
+    props.fields.SessionizeUrl.value ? props.fields.SessionizeUrl.value : null,
+    fetcher
   );
 
   if (!props?.fields?.SessionizeUrl?.value) {
@@ -55,31 +44,12 @@ const AgendaComponent = (props: AgendaProps): JSX.Element => {
   }
 
   return (
-      <div className={`component agenda ${props.params.styles}`} id={id ? id : undefined}>
-        <div className="component-content">
-          <InnerHTML html={data} />
-        </div>
+    <div className={`component agenda ${props.params.styles}`} id={id ? id : undefined}>
+      <div className="component-content">
+        <InnerHTML html={data} />
       </div>
+    </div>
   );
 };
 
-export const getServerSideProps: GetServerSideComponentProps = async (_, layoutData, context) => {
-  console.log('getServerSideProps');
-  // console.log(layoutData, layoutData);
-  // console.log(context, context);
-  return {
-    assetDetailsServer: 'Agenda test server',
-  };
-};
-
-export const getStaticProps: GetStaticComponentProps = async (_, layoutData, context) => {
-  console.log('getStaticProps');
-  // console.log(layoutData, layoutData);
-  // console.log(context, context);
-  return {
-    assetDetailsStatic: 'Agenda test',
-  };
-};
-
 export const Default = withDatasourceCheck()<AgendaProps>(AgendaComponent);
-
--- src/Project/Sugcon2024/Sugcon/src/pages/[[...path]].tsx
+++ src/Project/Sugcon2024/Sugcon/src/pages/[[...path]].tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { GetServerSideProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import NotFound from 'src/NotFound';
 import Layout from 'src/Layout';
 import {
@@ -7,11 +7,13 @@ import {
   SitecoreContext,
   ComponentPropsContext,
   EditingComponentPlaceholder,
+  StaticPath,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
 import { componentBuilder } from 'temp/componentBuilder';
+import { sitemapFetcher } from 'lib/sitemap-fetcher';
 
 const SitecorePage = ({
   notFound,
@@ -31,34 +33,73 @@ const SitecorePage = ({
 
   const isEditing = layoutData.sitecore.context.pageEditing;
   const isComponentRendering =
-      layoutData.sitecore.context.renderingType === RenderingType.Component;
+    layoutData.sitecore.context.renderingType === RenderingType.Component;
 
   return (
-      <ComponentPropsContext value={componentProps}>
-        <SitecoreContext
-            componentFactory={componentBuilder.getComponentFactory({ isEditing })}
-            layoutData={layoutData}
-        >
-          {/* Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing. */}
-          {isComponentRendering ? (
-              <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
-          ) : (
-              <Layout layoutData={layoutData} headLinks={headLinks} />
-          )}
-        </SitecoreContext>
-      </ComponentPropsContext>
+    <ComponentPropsContext value={componentProps}>
+      <SitecoreContext
+        componentFactory={componentBuilder.getComponentFactory({ isEditing })}
+        layoutData={layoutData}
+      >
+        {/*
+          Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing.
+          If you are using Experience Editor only, this logic can be removed, Layout can be left.
+        */}
+        {isComponentRendering ? (
+          <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
+        ) : (
+          <Layout layoutData={layoutData} headLinks={headLinks} />
+        )}
+      </SitecoreContext>
+    </ComponentPropsContext>
   );
 };
 
-// This function gets called at request time for SSR
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Fetching the props using sitecorePagePropsFactory for SSR
+// This function gets called at build and export time to determine
+// pages for SSG ("paths", as tokenized array).
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  // Fallback, along with revalidate in getStaticProps (below),
+  // enables Incremental Static Regeneration. This allows us to
+  // leave certain (or all) paths empty if desired and static pages
+  // will be generated on request (development mode in this example).
+  // Alternatively, the entire sitemap could be pre-rendered
+  // ahead of time (non-development mode in this example).
+  // See https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+
+  let paths: StaticPath[] = [];
+  let fallback: boolean | 'blocking' = 'blocking';
+
+  if (process.env.NODE_ENV !== 'development' && !process.env.DISABLE_SSG_FETCH) {
+    try {
+      // Note: Next.js runs export in production mode
+      paths = await sitemapFetcher.fetch(context);
+    } catch (error) {
+      console.log('Error occurred while fetching static paths');
+      console.log(error);
+    }
+
+    fallback = process.env.EXPORT_MODE ? false : fallback;
+  }
+
+  return {
+    paths,
+    fallback,
+  };
+};
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation (or fallback) is enabled and a new request comes in.
+export const getStaticProps: GetStaticProps = async (context) => {
   const props = await sitecorePagePropsFactory.create(context);
 
   return {
     props,
-    // Return custom 404 page with status code of 404 when `notFound` is true
-    notFound: props.notFound,
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    revalidate: 5, // In seconds
+    notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
   };
 };
 
