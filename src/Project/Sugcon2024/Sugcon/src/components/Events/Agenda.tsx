b199ba3bbfbb9563f7335178f56717b4b49d26b2
Jeroen Breuer <jbreuer@gmail.com>
2024-09-27 17:19:11 +0200

    Reapply "Experiment to use SSR."
    This reverts commit 204e67cad635a0cfccbb6dc9684e55f28f13f514.

--- src/Project/Sugcon2024/Sugcon/src/components/Events/Agenda.tsx
+++ src/Project/Sugcon2024/Sugcon/src/components/Events/Agenda.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import InnerHTML from 'dangerously-set-html-content';
-import { Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  Field,
+  GetServerSideComponentProps, GetStaticComponentProps,
+  useComponentProps,
+  withDatasourceCheck
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import useSWR from 'swr';
 import { ComponentProps } from 'lib/component-props';
 
@@ -13,20 +18,26 @@ type AgendaProps = ComponentProps & {
 };
 
 const AgendaDefaultComponent = (props: AgendaProps): JSX.Element => (
-  <div className={`component promo ${props.params.styles}`}>
-    <div className="component-content">
-      <span className="is-empty-hint">Agenda</span>
+    <div className={`component promo ${props.params.styles}`}>
+      <div className="component-content">
+        <span className="is-empty-hint">Agenda</span>
+      </div>
     </div>
-  </div>
 );
 
 const AgendaComponent = (props: AgendaProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
+  console.log('props.rendering.uid', props.rendering.uid);
+  
+  const componentProps = useComponentProps(props.rendering.uid) || {};
+
+  console.dir(componentProps, { depth: null });
+
   const fetcher = (url: string) => fetch(url).then((res) => res.text());
   const { data, error } = useSWR(
-    props.fields.SessionizeUrl.value ? props.fields.SessionizeUrl.value : null,
-    fetcher
+      props.fields.SessionizeUrl.value ? props.fields.SessionizeUrl.value : null,
+      fetcher
   );
 
   if (!props?.fields?.SessionizeUrl?.value) {
@@ -44,12 +55,31 @@ const AgendaComponent = (props: AgendaProps): JSX.Element => {
   }
 
   return (
-    <div className={`component agenda ${props.params.styles}`} id={id ? id : undefined}>
-      <div className="component-content">
-        <InnerHTML html={data} />
+      <div className={`component agenda ${props.params.styles}`} id={id ? id : undefined}>
+        <div className="component-content">
+          <InnerHTML html={data} />
+        </div>
       </div>
-    </div>
   );
 };
 
+export const getServerSideProps: GetServerSideComponentProps = async (_, layoutData, context) => {
+  console.log('getServerSideProps');
+  // console.log(layoutData, layoutData);
+  // console.log(context, context);
+  return {
+    assetDetailsServer: 'Agenda test server',
+  };
+};
+
+export const getStaticProps: GetStaticComponentProps = async (_, layoutData, context) => {
+  console.log('getStaticProps');
+  // console.log(layoutData, layoutData);
+  // console.log(context, context);
+  return {
+    assetDetailsStatic: 'Agenda test',
+  };
+};
+
 export const Default = withDatasourceCheck()<AgendaProps>(AgendaComponent);
+
